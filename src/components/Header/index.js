b6a4282a12ac75613ca05896e5ce50bc68daf738
karungituser <karunkumarkkd@gmail.com>
2022-06-29 05:46:41 +0000

    Routing Practice tests Solution

---
+++ src/components/Header/index.js
@@ -0,0 +1,33 @@
+// Write your JS code here
+import {Link} from 'react-router-dom'
+import './index.css'
+
+const Header = () => (
+  <nav className="nav-header">
+    <div className="nav-header-container">
+      <div className="waveImage-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/wave-logo-img.png"
+          alt="wave"
+          className="wave-image"
+        />
+        <p className="wave">Wave</p>
+      </div>
+      <div className="nav-link-container">
+        <ul className="list-container">
+          <li className="nav-link">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="nav-link">
+            <Link to="/about">About</Link>
+          </li>
+          <li className="nav-link">
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+)
+
+export default Header
